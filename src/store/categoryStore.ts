e9a20ecdace43188d16dea52e2c48ea6fe4008c4
zhou <648926954@qq.com>
2025-08-13 12:28:11 +0800

    首页

--- src/store/categoryStore.ts
+++ src/store/categoryStore.ts
@@ -4,36 +4,38 @@ import httpInstance from "../requests/http.ts";
 
 
 export const categoryStore = defineStore("categoryStore", () => {
-    interface FirstCateType {
-        id: number;
-        name: string;
-    }
-
-    interface cateType {
+    interface SecondCateType {
         id: number;
         category_name: string;
-        image: string;
         url: string;
+        image: string;
+        first_category: number;
+    }
+
+    interface FirstCateType {
+        id: number;
+        secondary_category: SecondCateType[];
+        name: string;
     }
     interface reqType {
         code: number;
         message: string;
         first_cate_name: string;
-        data: cateType[];
+        data: FirstCateType[];
         success: boolean;
     }
 
     const categoryList = ref<reqType>();
-    const cateList = ref<cateType[]>();
+    const FirstCateList = ref<FirstCateType[]>();
 
     const getCategoryList = async () => {
         const res = await httpInstance.get<reqType>("v1/cate/ls");
         categoryList.value = res.data;
-        cateList.value = res.data.data;
+        FirstCateList.value = res.data.data;
         console.log(categoryList.value);
     }
     return {
-        cateList,
+        FirstCateList,
         categoryList,
         getCategoryList,
     }
--- src/views/home/HomeCategory.vue
+++ src/views/home/HomeCategory.vue
@@ -13,29 +13,12 @@ const router = useRouter();
 onMounted(async ()=>{
   await cateStore.getCategoryList()
   if(cateStore.categoryList.success) {
-    listData.value = cateStore.cateList
-    console.log(cateStore.categoryList.first_cate_name)
+    listData.value = cateStore.FirstCateList;
+    console.log(listData.value);
   }
 
 })
 
-// 按 first_category.id 分组，返回一个对象，key 是 first_category.id
-const groupedData = computed(() => {
-  return listData.value.reduce((acc, item) => {
-    const key = item.first_category.id;
-    if (!acc[key]) {
-      acc[key] = {
-        name: item.first_category.name,
-        categories: []
-      };
-    }
-    acc[key].categories.push(item);
-    return acc;
-  }, {} as Record<
-      number,
-      { name: string; categories: typeof listData.value }
-  >);
-});
 
 const goConcrete = (itemId) => {
   router.push(`/concrete/${itemId}`)
@@ -43,10 +26,10 @@ const goConcrete = (itemId) => {
 </script>
 
 <template>
-  <div class="product-grid" v-for="(group, key) in groupedData" :key="key">
-    <h2>{{group.name}}</h2>
+  <div class="product-grid" v-for="first_cate in listData" :key="first_cate.id">
+    <h2>{{first_cate.name}}</h2>
     <div class="products">
-      <div class="product-card" v-for="item in listData" :key="item.id" @click="goConcrete(item.id)">
+      <div class="product-card" v-for="item in first_cate.secondary_category" :key="item.id" @click="goConcrete(item.id)">
         <img :src="`http://localhost:8000${item.image}`" style="height: 300px" alt="男士上衣" />
         <h3>{{item.category_name}}</h3>
 <!--        <p>{{item}}</p>-->
